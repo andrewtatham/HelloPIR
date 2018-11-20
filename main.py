3916794027938a84367c85c7da1e646526c38d21
Andrew Tatham <andrewtatham@gmail.com>
2018-11-20 11:27:46 +0000

    added blinkstick

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import time
-
+from blinkstick import blinkstick
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 PIR_PIN = 4
 GPIO.setup(PIR_PIN, GPIO.IN)
 
+bs = blinkstick.find_first()
+
 
 def MOTION(pir_pin):
     print('Motion Detected!')
+    bs.set_random_color()
+    time.sleep(0.5)
+    bs.turn_off()
 
 
 print('PIR Module Test (CTRL+C to exit)')
@@ -18,7 +23,8 @@ print('Ready')
 try:
     GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
     while True:
-        time.sleep(100)
+        time.sleep(1)
+
 except KeyboardInterrupt:
     print('Quit')
     GPIO.cleanup()

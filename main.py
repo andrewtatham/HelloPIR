d873c5a7cb1e07e9482300596d4729a2eb7d6b56
Andrew Tatham <andrewtatham@gmail.com>
2018-11-20 17:07:30 +0000

    refactor into classes

--- main.py
+++ main.py
@@ -3,79 +3,88 @@ from blinkstick import blinkstick
 import RPi.GPIO as GPIO
 import datetime
 
-GPIO.setmode(GPIO.BCM)
-PIR_PIN = 4  # NB BCM PIN NUMBNER
-GPIO.setup(PIR_PIN, GPIO.IN)
 
-bs = blinkstick.find_first()
+class MyPIR(object):
+    def __init__(self):
+        self.state = False
+        self.state_at = None
 
-state = False
-state_at = None
+    def initialize(self):
+        GPIO.setmode(GPIO.BCM)
+        PIR_PIN = 4  # NB BCM PIN NUMBER
+        GPIO.setup(PIR_PIN, GPIO.IN)
 
+        print('PIR Module Test (CTRL+C to exit)')
+        time.sleep(2)
+        print('Ready')
 
-def light_on():
-    print('light_on')
-    try:
-        bs.set_random_color()
-    except Exception as e:
-        print(e)
+        GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=self.callback)
 
+    def hello(self):
+        print('hello')
+        self.state = True
 
-def light_off():
-    print('light_off')
-    try:
-        bs.turn_off()
-    except Exception as e:
-        print(e)
+    def goodbye(self):
+        print('goodbye')
+        self.state = False
+        self.state_at = None
 
+    def callback(self, pir_pin):
+        print('callback')
+        self.state_at = datetime.datetime.utcnow()
+        if not self.state:
+            self.hello()
 
-def hello():
-    global state, state_at
-    print('hello')
-    state = True
-    light_on()
+    def detect_timeout(self):
+        if self.state and self.state_at:
+            last_detection = datetime.datetime.utcnow() - self.state_at
+            seconds = last_detection.seconds
+            print('seconds: {}'.format(seconds))
+            if seconds > 300:
+                self.goodbye()
 
+    def run(self):
+        try:
+            while True:
+                self.detect_timeout()
+                print('state: {}'.format(self.state))
+                time.sleep(1)
 
-def goodbye():
-    global state, state_at
-    print('goodbye')
-    state = False
-    state_at = None
-    light_off()
-
-
-def callback(pir_pin):
-    global state, state_at
-    print('callback')
-    state_at = datetime.datetime.utcnow()
-    if not state:
-        hello()
-
-
-def detect_timeout():
-    global state, state_at
-    if state and state_at:
-        last_detection = datetime.datetime.utcnow() - state_at
-        seconds = last_detection.seconds
-        print('seconds: {}'.format(seconds))
-        if seconds > 300:
-            goodbye()
+        except KeyboardInterrupt:
+            print('Quit')
+        finally:
+            self.goodbye()
+            GPIO.cleanup()
 
 
-print('PIR Module Test (CTRL+C to exit)')
-time.sleep(2)
-print('Ready')
+class BlinkstickPIR(MyPIR):
+    def __init__(self):
+        super().__init__()
+        self.bs = blinkstick.find_first()
 
-GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=callback)
+    def hello(self):
+        super().hello()
+        self.light_on()
 
-try:
-    while True:
-        detect_timeout()
-        print('state: {}'.format(state))
-        time.sleep(1)
+    def goodbye(self):
+        super().goodbye()
+        self.light_off()
 
-except KeyboardInterrupt:
-    print('Quit')
-    GPIO.cleanup()
-finally:
-    light_off()
+    def light_on(self):
+        print('light_on')
+        try:
+            self.bs.set_random_color()
+        except Exception as e:
+            print(e)
+
+    def light_off(self):
+        print('light_off')
+        try:
+            self.bs.turn_off()
+        except Exception as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    pir = BlinkstickPIR()
+    pir.run()

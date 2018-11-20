87f818b876ebb9a123dbb35c93e70b4e9a88f9d9
Andrew Tatham <andrewtatham@gmail.com>
2018-11-20 12:44:34 +0000

    refactor

--- main.py
+++ main.py
@@ -29,24 +29,37 @@ def light_off():
         print(e)
 
 
-def callback(pir_pin):
-    global state, state_at
-    print('callback')
-    if not state:
-        hello()
-    # state = GPIO.input(PIR_PIN)
-    state = True
-    state_at = datetime.datetime.utcnow()
-
-
 def hello():
+    global state, state_at
     print('hello')
+    state = True
     light_on()
 
 
 def goodbye():
+    global state, state_at
     print('goodbye')
+    state = False
+    state_at = None
     light_off()
+
+
+def callback(pir_pin):
+    global state, state_at
+    print('callback')
+    state_at = datetime.datetime.utcnow()
+    if not state:
+        hello()
+
+
+def detect_timeout():
+    global state, state_at
+    if state and state_at:
+        last_detection = datetime.datetime.utcnow() - state_at
+        seconds = last_detection.seconds
+        print('seconds: {}'.format(seconds))
+        if seconds > 15:
+            goodbye()
 
 
 print('PIR Module Test (CTRL+C to exit)')
@@ -54,16 +67,10 @@ time.sleep(2)
 print('Ready')
 
 GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=callback)
+
 try:
     while True:
-        if state and state_at:
-            last_detection = datetime.datetime.utcnow() - state_at
-            seconds = last_detection.seconds
-            print('seconds: {}'.format(seconds))
-            if seconds > 15:
-                state = False
-                state_at = None
-                goodbye()
+        detect_timeout()
         print('state: {}'.format(state))
         time.sleep(1)
 

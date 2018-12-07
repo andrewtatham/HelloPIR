cdf0c00404aa8bcabb2bfcaa02c337c2c1fbc3f5
Andrew Tatham <andrewtatham@gmail.com>
2018-12-07 01:21:23 +0000

    hsv

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import colorsys
+import datetime
 import time
 from blinkstick import blinkstick
 import RPi.GPIO as GPIO
-import datetime
 
 
 class MyPIR(object):
@@ -25,6 +26,9 @@ class MyPIR(object):
 
     def movement(self):
         print('movement')
+
+    def tick(self):
+        print('tick')
 
     def goodbye(self):
         print('goodbye')
@@ -66,6 +70,8 @@ class BlinkstickPIR(MyPIR):
     def __init__(self):
         super(BlinkstickPIR, self).__init__()
         self.bs = blinkstick.find_first()
+        self.light_off()
+        self._hsv = (0, 0, 0)
 
     def hello(self):
         super(BlinkstickPIR, self).hello()
@@ -75,6 +81,10 @@ class BlinkstickPIR(MyPIR):
         super(BlinkstickPIR, self).movement()
         self.light_blink()
 
+    def tick(self):
+        super(BlinkstickPIR, self).tick()
+        self.light_dim()
+
     def goodbye(self):
         super(BlinkstickPIR, self).goodbye()
         self.light_off()
@@ -82,23 +92,52 @@ class BlinkstickPIR(MyPIR):
     def light_on(self):
         print('light_on')
         try:
-            self.bs.set_color(0, 0, 0, 8)
+            h, s, v = self._hsv
+            v = 0.5
+            self._hsv = (h, s, v)
+            self.set_colour()
         except Exception as e:
             print(e)
 
     def light_blink(self):
         print('light_blink')
         try:
-            self.bs.blink(0, 1, 8)
+            h, s, v = self._hsv
+            v = 1.0
+            self._hsv = (h, s, v)
+            self.set_colour()
+            time.sleep(1)
+        except Exception as e:
+            print(e)
+
+    def light_dim(self):
+        print('light_dim')
+        try:
+            h, s, v = self._hsv
+            h = (h + 0.01) % 1.0
+            v = max(0.0, v * 0.85)
+            self._hsv = (h, s, v)
+            self.set_colour()
         except Exception as e:
             print(e)
 
     def light_off(self):
         print('light_off')
         try:
+            h, s, v = self._hsv
+            v = 0.0
+            self._hsv = (h, s, v)
+            self.set_colour()
             self.bs.turn_off()
         except Exception as e:
             print(e)
+
+    def set_colour(self):
+        h, s, v = self._hsv
+        r, g, b = colorsys.hsv_to_rgb(h, s, v)
+        print("Setting Colour rgb: {}, hsv: {}".Format((r, g, b), (h, s, v)))
+        self.bs.set_color(0, 0, r, g, b)
+        self.bs.set_color(0, 1, r, g, b)
 
 
 if __name__ == '__main__':
